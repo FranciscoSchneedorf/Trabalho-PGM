f082f15de6dda204eeaee41f75ee63aebee13c62
Maick <maickdzn@gmail.com>
2026-06-25 11:13:34 -0300

    Pointer Test

--- imagens.cpp
+++ imagens.cpp
@@ -28,7 +28,7 @@ int carregaPGM(string nome, tImagem img, int *col, int *lin, int *tons)
 
     // Leitura do arquivo
     arquivo >> *col >> *lin >> *tons;
-    for (int *p = &img[0][0]; p < &img[0][0] + (*lin) * (*col); p++)
+    for (int *p = &img[0][0]; *p < &img[0][0] + (*lin) * (*col); p++)
     {
         arquivo >> *p;
     }
@@ -56,9 +56,9 @@ int salvaPGM(string nome, tImagem img, int col, int lin, int tons)
             << tons << endl;
 
     // Salvando o arquivo
-for (int *p = &img[0][0]; p < &img[0][0] + (*lin) * (*col); p++)
+for (int *p = &img[0][0]; *p < &img[0][0] + (*lin) * (*col); p++)
     {
-            arquivo << img[i][j] << " ";
+            arquivo << *P << " ";
         arquivo << endl;
     }
 
